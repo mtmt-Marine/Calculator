fba1dc7abcc4939ec75535500bdd702e8e8ab593
mtmt-Marine <56342199+mtmt-Marine@users.noreply.github.com>
2020-07-09 12:02:56 +0900

    フォルダ分け

---
+++ Calculator/Command/CalculatorCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator.Command
+{
+    internal class CalculatorCommand : ICommand
+    {
+        private Action _action;
+
+        public CalculatorCommand(Action action)
+        {
+            this._action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _action();
+    }
+}
