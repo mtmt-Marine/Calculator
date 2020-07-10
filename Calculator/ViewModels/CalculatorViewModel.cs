8a54c92c3f4d84dc7aae0cbe57c62c593c06956d
mtmt-Marine <56342199+mtmt-Marine@users.noreply.github.com>
2020-07-10 13:09:51 +0900

    ReactiveProperty, Prismの追加

--- Calculator/ViewModels/CalculatorViewModel.cs
+++ Calculator/ViewModels/CalculatorViewModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Reactive.Bindings;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System;
+using Prism.Mvvm;
 
 namespace Calculator.ViewModels
 {
-    class CalculatorViewModel
+    internal class CalculatorViewModel : BindableBase
     {
+        public ReactiveProperty<string> InputNumber { get; private set; }
+        public ReactiveProperty<string> InputSign { get; private set; }
+        public ReactiveCommand NumberButton { get; private set; }
+        public ReactiveCommand SignButton { get; } = new ReactiveCommand();
+        public ReactiveCommand ClearButton { get; private set; }
+
+        public CalculatorViewModel()
+        {
+            this.InputNumber = new ReactiveProperty<string>("0");
+            this.InputSign = new ReactiveProperty<string>("");
+
+            this.SignButton = new ReactiveCommand();
+            this.SignButton.Subscribe(_ => InputSign.Value = "+");
+        }
     }
 }
